612d1c3fdb77d3df8e8e637f8bf178c5bdbecc79
Abhijit Roy <aroy30@uic.edu>
2019-01-16 01:08:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,13 @@ end
 
 # Asks the user for move recursively
 def turn(board)
-
-
-
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
